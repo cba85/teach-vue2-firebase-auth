81d510e94336018617bac5c11c7598d0c06c2e64
Clément Barbaza <cba85@users.noreply.github.com>
2021-03-30 17:20:49 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -10,15 +10,7 @@ Vue.use(Vuelidate);
 
 require("./assets/scss/app.scss");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyBe3ksGrtgKQVGF4bWVcgXCVtxwpwM3kdM",
-  authDomain: "vue-firebase-auth-18022.firebaseapp.com",
-  databaseURL: "https://vue-firebase-auth-18022.firebaseio.com",
-  projectId: "vue-firebase-auth-18022",
-  storageBucket: "vue-firebase-auth-18022.appspot.com",
-  messagingSenderId: "1055421470615",
-  appId: "1:1055421470615:web:f0c0f771489c5ec461d6d2"
-};
+var firebaseConfig = {};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
